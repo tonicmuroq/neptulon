0f5582fcf4c6b66153517313e7d1c1177d0163a6
CMGS <ilskdw@gmail.com>
2015-12-16 17:36:27 +0800

    support eru host mode

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 
+from neptulon.config import SERVER_PORT, DEBUG
 from neptulon.app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)
+    app.run('0.0.0.0', SERVER_PORT, debug=DEBUG)
